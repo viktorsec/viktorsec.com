ca3552fc6274ac67dbbfd69607d192dd5d7af54c
Viktor Sec <hey@viktorsec.com>
2017-05-29 23:44:27 +0200

    Swap paypal for ether

--- index.php
+++ index.php
@@ -129,93 +129,93 @@
     <div id="site">
       <div class="mainframe">
         <div id="header">
-					<div class="flex">
-						<img class="avatar" src="graphics/myself.png" width="128" height="128" alt="Viktor Sec's avatar" />
-						<h1>Hi, I'm Viktor Seč</h1>
-					</div>
-					<p class="bio">
-						Software Engineer at&nbsp;<a href="https://vacuumlabs.com/">Vacuumlabs</a>,<br />
-						Cognitive Science Master's Student at&nbsp;<a href="http://fmph.uniba.sk/">Comenius University</a>,
-						Maker & Lector at&nbsp;<a href="http://lab.cafe/">Lab</a>
-					</p>
-				</div>
+          <div class="flex">
+            <img class="avatar" src="graphics/myself.png" width="128" height="128" alt="Viktor Sec's avatar" />
+            <h1>Hi, I'm Viktor Seč</h1>
+          </div>
+          <p class="bio">
+            Software Engineer at&nbsp;<a href="https://vacuumlabs.com/">Vacuumlabs</a>,<br />
+            Cognitive Science Master's Student at&nbsp;<a href="http://fmph.uniba.sk/">Comenius University</a>,
+            Maker & Lector at&nbsp;<a href="http://lab.cafe/">Lab</a>
+          </p>
+        </div>
         <div id="list">
-      <ul>
-		    <li class="mail">
-			    <a href="mailto:hey@viktorsec.com">hey@viktorsec.com</a>
-		    </li>
-        <li class="linkedin">
-          <a href="https://sk.linkedin.com/in/yorulis">LinkedIn</a>
-        </li>
-				<li class="github">
-					<a href="https://github.com/viktorsec">GitHub</a>
-				</li>
-        <li class="facebook">
-          <a href="http://facebook.com/yorulis">Facebook</a>
-        </li>
-        <li class="youtube">
-          <a href="http://youtube.com/YregYorulis">YouTube</a>
-        </li>
-        <li class="vimeo">
-          <a href="https://vimeo.com/yreg">Vimeo</a>
-        </li>
-        <li class="tumblr">
-          <a href="http://blog.viktorsec.com/">tumblr</a>
-        </li>
-        <li class="twitter">
-          <a href="http://twitter.com/yorulis">Twitter</a>
-        </li>
-        <li class="spotify">
-          <a href="https://open.spotify.com/user/yorulis">Spotify</a>
-        </li>
-        <li class="lastfm">
-          <a href="http://www.last.fm/user/Yreg">last.fm</a>
-        </li>
-        <li class="goodreads">
-          <a href="https://www.goodreads.com/yreg">goodreads</a>
-        </li>
-        <li class="instagram">
-          <a href="https://www.instagram.com/vividpeanut/">Instagram</a>
-        </li>
-        <li class="geocaching">
-          <a href="http://www.geocaching.com/seek/nearest.aspx?ul=yregyorulis">Geocaching</a>
-        </li>
-        <li class="reddit">
-          <a href="https://www.reddit.com/user/yreg">reddit</a>
-        </li>
-        <li class="steam">
-          <a href="http://steamcommunity.com/id/yreg/">Steam</a>
-        </li>
-        <li class="cucumbertown">
-          <a href="http://yreg.cucumbertown.com/">Cucumbertown</a>
-        </li>
-        <li class="oldprojects">
-          <a href="http://yreg.me/hi.php">old projects</a>
-        </li>
-        <li class="netiketa">
-          <a href="http://netiketa.yreg.me/home.php">Netiketa</a>
-        </li>
-      </ul>
+          <ul>
+            <li class="mail">
+              <a href="mailto:hey@viktorsec.com">hey@viktorsec.com</a>
+            </li>
+            <li class="linkedin">
+              <a href="https://sk.linkedin.com/in/yorulis">LinkedIn</a>
+            </li>
+            <li class="github">
+              <a href="https://github.com/viktorsec">GitHub</a>
+            </li>
+            <li class="facebook">
+              <a href="http://facebook.com/yorulis">Facebook</a>
+            </li>
+            <li class="youtube">
+              <a href="http://youtube.com/YregYorulis">YouTube</a>
+            </li>
+            <li class="vimeo">
+              <a href="https://vimeo.com/yreg">Vimeo</a>
+            </li>
+            <li class="tumblr">
+              <a href="http://blog.viktorsec.com/">tumblr</a>
+            </li>
+            <li class="twitter">
+              <a href="http://twitter.com/yorulis">Twitter</a>
+            </li>
+            <li class="spotify">
+              <a href="https://open.spotify.com/user/yorulis">Spotify</a>
+            </li>
+            <li class="lastfm">
+              <a href="http://www.last.fm/user/Yreg">last.fm</a>
+            </li>
+            <li class="goodreads">
+              <a href="https://www.goodreads.com/yreg">goodreads</a>
+            </li>
+            <li class="instagram">
+              <a href="https://www.instagram.com/vividpeanut/">Instagram</a>
+            </li>
+            <li class="geocaching">
+              <a href="http://www.geocaching.com/seek/nearest.aspx?ul=yregyorulis">Geocaching</a>
+            </li>
+            <li class="reddit">
+              <a href="https://www.reddit.com/user/yreg">reddit</a>
+            </li>
+            <li class="steam">
+              <a href="http://steamcommunity.com/id/yreg/">Steam</a>
+            </li>
+            <li class="cucumbertown">
+              <a href="http://yreg.cucumbertown.com/">Cucumbertown</a>
+            </li>
+            <li class="oldprojects">
+              <a href="http://yreg.me/hi.php">old projects</a>
+            </li>
+            <li class="netiketa">
+              <a href="http://netiketa.yreg.me/home.php">Netiketa</a>
+            </li>
+          </ul>
         </div>
       </div> <!-- /mainframe -->
     </div><!-- /site -->
-	<div id="site-bank">
-    <div class="mainframe">
-      <ul>
-        <li>
-          IBAN: SK69 1100 0000 0026 1946 1176
-        </li>
-        <li>
-          Bitcoin: 1YregghjnUei789Gtxw8bEN4LyJifV8nK
-        </li>
-        <li>
-          PayPal: <a href="https://www.paypal.com/sk/webapps/mpp/send-money-online">viktor.sec@gmail.com</a>
-        </li>
-		<li>
-		  <p class="small">ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQC3B5pl7Ge8UWj9aaiJESGxwIIUhqQO4sGMnydSofh7vx/SzxF0Q+WRg0UoQ2KMcaiTnPul6WqT53V5c3sIH/fURa0VF8WTjOEm5d2Hm06YgLem0NNPIe2IXxuObAsAQ4+csANmXD7ht5Fz10Jtdo2kHSbWAIeFAI7OFfgBXNLRwQ== Viktor_Sec</p>
-		</li>
-      </ul>
+    <div id="site-bank">
+      <div class="mainframe account">
+        <ul>
+          <li class="iban">
+            IBAN: SK69 1100 0000 0026 1946 1176
+          </li>
+          <li>
+            Bitcoin: 1YregghjnUei789Gtxw8bEN4LyJifV8nK
+          </li>
+          <li>
+            Ethereum: 0x0bded8db0e07fda30904ac3849a0fd61a657692d
+          </li>
+          <li>
+            <p class="small">ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQC3B5pl7Ge8UWj9aaiJESGxwIIUhqQO4sGMnydSofh7vx/SzxF0Q+WRg0UoQ2KMcaiTnPul6WqT53V5c3sIH/fURa0VF8WTjOEm5d2Hm06YgLem0NNPIe2IXxuObAsAQ4+csANmXD7ht5Fz10Jtdo2kHSbWAIeFAI7OFfgBXNLRwQ== Viktor_Sec</p>
+          </li>
+        </ul>
+      </div>
     </div>
-	</div>
   </body>
 </html>
